30e01757899e243329ca856b52589bdbe5f4c14a
CptDenev <baptiste.pin@gmail.com>
2026-08-18 15:48:47 +0200

    003 : clean source
    split code in function and clean a bit

--- 001_ImageClassifier.py
+++ 001_ImageClassifier.py
@@ -16,61 +16,74 @@ def search_img(term, max_images=10):
 
 
 
-#create data set
-searches = 'bird','forest'
-path = Path('bird_or_not')
-
-for o in searches:
-    dest = (path/o)
-    dest.mkdir(exist_ok=True, parents=True)
-    download_images(dest, urls=search_img(f'{o} photo'))
-    time.sleep(2)
-    download_images(dest, urls=search_img(f'{o} sun photo'))
-    time.sleep(2)
-    download_images(dest, urls=search_img(f'{o} shade photo'))
-    time.sleep(2)
-    #resize images
-    resize_images(path/o, max_size=400, dest=path/o, max_workers=0, recurse=True)
+def downloadDataSet(searches, path):
+    for o in searches:
+            dest = (path/o)
+            dest.mkdir(exist_ok=True, parents=True)
+            download_images(dest, urls=search_img(f'{o} photo'))
+            time.sleep(2)
+            download_images(dest, urls=search_img(f'{o} sun photo'))
+            time.sleep(2)
+            download_images(dest, urls=search_img(f'{o} shade photo'))
+            time.sleep(2)
+            #resize images
+            resize_images(path/o, max_size=400, dest=path/o, max_workers=0, recurse=True)
 
 
+def cleanDataSet(path):
+    fns = get_image_files(path)
+    #print(fns)
+    
+    #clean file with verify_image and not verify_images as 64 cores break code
+    for fn in fns:
+        passed = verify_image(fn)
+        if not passed:
+            os.unlink(fn)
 
 
-print("set downloaded")
-fns = get_image_files(path)
-print(fns)
-'''
-#clean file with verify_image and not verify_images as 64 cores break code
-for fn in fns:
-    passed = verify_image(fn)
-    if not passed:
-        os.unlink(fn)
-'''
+def trainOnDataSet(dls, epoch=3):
+    learn = vision_learner(dls, resnet18, metrics=error_rate)
+    learn.fine_tune(epoch)
+    return learn
 
 
-
-#create DataBlock
-dls = DataBlock(
-    #input are images block, outputs are categories
-    blocks=(ImageBlock, CategoryBlock),
-    #get all images from path
-    get_items=get_image_files,
-    #keep 20% for cross validation
-    splitter=RandomSplitter(valid_pct=0.2, seed=42),
-    #set y value as the name of our folder
-    get_y=parent_label,
-    #rseize image to 192*192 and squish them not crop to keep all informations
-    item_tfms=[Resize(192, method='squish')]
-).dataloaders(path, bs=32, num_workers=0)
-
-#dls.show_batch(max_n=6)
+def main():
+    #create data set
+    searches = 'bird','forest'
+    path = Path('bird_or_not')
 
 
+    downloadDataSet(searches, path)
+    print("data set downloaded")
 
-#train the model on resnet18
-learn = vision_learner(dls, resnet18, metrics=error_rate)
-learn.fine_tune(3)
+    cleanDataSet(path)
+    print("data set cleaned")
 
-#try model on local bird.jpg
-is_bird,_,probs = learn.predict(PILImage.create('bird.jpg'))
-print(f"This is a: {is_bird}.")
-print(f"Probability it's a bird: {probs[0]:.4f}")
+
+    #create DataBlock, need num_workers fixed to avoid core > 63 causing crash
+    dls = DataBlock(
+        #input are images block, outputs are categories
+        blocks=(ImageBlock, CategoryBlock),
+        #get all images from path
+        get_items=get_image_files,
+        #keep 20% for cross validation
+        splitter=RandomSplitter(valid_pct=0.2, seed=42),
+        #set y value as the name of our folder
+        get_y=parent_label,
+        #rseize image to 192*192 and squish them not crop to keep all informations
+        item_tfms=[Resize(192, method='squish')]
+    ).dataloaders(path, bs=32, num_workers=0)
+
+    #dls.show_batch(max_n=6)
+
+    #train the model on resnet18 with 3 epoch
+    learn = trainOnDataSet(dls, 3)
+
+    #try model on local bird.jpg
+    is_bird,_,probs = learn.predict(PILImage.create('bird.jpg'))
+    print(f"This is a: {is_bird}.")
+    print(f"Probability it's a bird: {probs[0]:.4f}")
+
+
+if __name__ == '__main__':
+    main()
