217897d60e1c9e937c4bf1175c7e92d24af10758
CptDenev <baptiste.pin@gmail.com>
2026-09-01 12:40:12 +0200

    021 : create MNIST loader and split set
    create loader, download if needed, convert to tensor and split in train, eval and test

--- 004_NumberMNIST.py
+++ 004_NumberMNIST.py
@@ -12,6 +12,7 @@ SEED = 33
 torch.manual_seed(SEED)
 np.random.seed(SEED)
 
+#check cuda or mps availability, else cpu
 if(torch.cuda.is_available()):
     device = torch.device("cuda")
 elif(torch.backends.mps.is_available()):
@@ -20,11 +21,39 @@ else:
     device = torch.device("cpu")
 
 
-print(f"Device on which we're running: {device}")
-print(torch.version.cuda)
+print(f"device on which we're running: {device}")
 
 BATCH_SIZE = 128
 EPOCHS = 20
 LR = 1e-3
 PATIENCE = 5
 DATA_DIR = "./dataset/mnist"
+
+
+#Load MNIST and convert to tensor
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.1307,),(0.3081,))
+])
+
+train_dataset = datasets.MNIST(root=DATA_DIR, train=True, download=True, transform=transform) 
+test_dataset = datasets.MNIST(root=DATA_DIR, train=False, download=True, transform=transform)
+
+print(f"train set : {len(train_dataset)} images")
+print(f"test set : {len(test_dataset)} images")
+
+
+#Split train in train + eval
+val_size = int(0.1 * len(train_dataset))
+train_size = len(train_dataset) - val_size
+
+train_subset, val_subset = random_split(
+    train_dataset,
+    [train_size, val_size],
+    generator = torch.Generator.manual_seed(SEED)
+)
+
+print(f"train set : {len(train_subset)} | validation set : {len(val_subset)} | test set {len(test_dataset)}")
+
+
+#define data loaders
