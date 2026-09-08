e7624baa932e4ed79908bd0b6000b02c33253470
CptDenev <baptiste.pin@gmail.com>
2026-09-08 15:20:33 +0200

    037 : wip on needed lib for project

--- 006_TabularMLPvsRF.py
+++ 006_TabularMLPvsRF.py
@@ -1,14 +1,24 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.utils.data import DataLoader, random_split
+from torch.utils.data import DataLoader, TensorDataset, random_split
 from torchvision import datasets, transforms
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix, f1_score
 import numpy as np
+import panda as pd
+import joblib
 import os
 
 #get CSV data set
 #clean data set
 #split data set in train, cross, test
 # 
-# define archi 
+#define archi MLP pytorch
+#define train ad save
+#
+#define archi RF sickitlearn
+#define train and save
+#
+#menu train, compare
