3fe9911906442146db81be046491318b816b5475
CptDenev <baptiste.pin@gmail.com>
2026-09-15 11:49:15 +0200

    059: add Up class to UNet archi

--- UNetModel.py
+++ UNetModel.py
@@ -36,10 +36,19 @@ class Down(nn.Module):
 
 """
     Upsample
-    
+    Add info learned from 2*2 on pixel then cat those info with the skip sotcked from down
 """
 class Up(nn.Module):
-    pass
+    def __init__(self, in_ch, out_ch):
+        super().__init__()
+        #we divide in_ch by 2 due to keep a stable channel dimension with the cat operation incoming
+        self.up = nn.ConvTranspose2d(in_ch, in_ch // 2, kernel_size=2, stride=2)
+        self.conv = DoubleConv(in_ch, out_ch)
+
+    def forward(self, x, skip):
+        x = self.up(x)
+        x=torch.cat([x, skip], dim=1)
+        return self.conv(x)
 
 
 class UNet(nn.Module):
