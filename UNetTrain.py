a6f788e97c2ff9cb0211aff7d2996a276f8ce35d
CptDenev <baptiste.pin@gmail.com>
2026-09-16 17:07:33 +0200

    064: base structure for training
    add LR and EPOCHS global parameters, create function for train one epoch, eval one epoch and training loop

--- UNetTrain.py
+++ UNetTrain.py
@@ -30,6 +30,8 @@ CLASS_NAME =[
 NUM_CLASSES = 8
 IGNORE_INDEX = 7
 IN_CHANNEL = 3
+UNET_LR = 0.001
+UNET_EPOCHS = 30
 
 #---Device detection---
 def getDevice():
@@ -48,11 +50,69 @@ def getDataSet():
     return train_ds, val_ds, test_ds
 
 
+def unet_train_one_epoch(model, loader, criterion, optimizer, device):
+    model.train()
+    total_loss = 0.0
+
+    for batch in loader:
+        images = batch['image'].to(device)
+        mask = batch['mask'].to(device)
+
+        optimizer.zero_grad()
+        logits = model(images)
+        loss = criterion(logits, mask)
+        loss.backward()
+        optimizer.step()
+
+        total_loss += loss.item() * images.size(0)
+
+    return total_loss / len(loader.dataset) 
+
+
+@torch.no_grad()
+def unet_evaluate(model, loader, criterion, device):
+    model.eval()
+    total_loss = 0.0
+
+    for batch in loader:
+        images = batch['image'].to(device)
+        mask = batch[mask].to(device)
+
+        logits = model(images)
+        loss = criterion(logits, mask)
+        total_loss += loss.item() * images.size(0)
+
+    return total_loss / len(loader.dataset)
+
+
+def unet_train(train_ds, val_ds, device):
+    torch.manual_seed(SEED)
+
+    train_dl = DataLoader(train_ds, batch_size=4, shuffle=True, num_workers=2)
+    val_dl = DataLoader(val_ds, batch_size=4, shuffle=False, num_workers=2)
+
+    #model definition
+    model = UNet(in_channels=IN_CHANNEL, num_classes=NUM_CLASSES, base_filters=64)
+    #loss function with ignore pixel given
+    criterion = nn.CrossEntropyLoss(ignore_index=IGNORE_INDEX)
+    optimizer = torch.optim.Adam(model.parameters(), lr=UNET_LR, weight_decay=1e-4)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', patience=3, factor=0.5)
+
+    for epoch in range(1, UNET_EPOCHS+1):
+        train_loss = unet_train_one_epoch(model, train_dl, criterion, optimizer, device)
+        val_loss =  unet_evaluate(model, val_dl, criterion, device)
+        #we update our LR based on val_loss
+        scheduler.step(val_loss)
+
+        print(f"epoch: {epoch:.3d} | train loss: {train_loss:3d} | val loss: {val_loss:3d}")
+
+
 #---Main---
 def main():
     device = getDevice()
     print(f"Device :{device}")
 
+    #get data set and check dtype and length
     train_ds, val_ds, test_ds = getDataSet()
     print(f"train : {len(train_ds)} | test : {len(test_ds)} | val : {len(val_ds)}")
 
